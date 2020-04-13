45033c09de57d8ac7a6f9b3ccf6c664b757b6a4d
lennygin <lennginzan@gmail.com>
2020-04-13 12:49:41 -0500

    checkpoint OR AND NOT working

--- model.h
+++ model.h
@@ -29,39 +29,50 @@ void OR(neural **Inicio)
     n = 0.5;
     temp->w[0] = 0.5;
     temp->w[1] = 0.5;
-    printf("Inserta tu 1era entrada(0 o 1):\n");
+    printf("Inserta tu 1era entrada(0 o 1): ");
     scanf("%f", &temp->x[0]);
-    printf("Inserta tu 2nda entrada(0 o 1):\n");
+    printf("Inserta tu 2nda entrada(0 o 1): ");
     scanf("%f", &temp->x[1]);
 
-    if (temp->x[0] == 0)
-        temp->x[0] = 0.001;
-    if (temp->x[1] == 0)
-        temp->x[1] = 0.001;
+    if ((temp->x[0] == 0) && (temp->x[1] == 0))
+    {
+        Y = 0;
+        temp->x[0] = 0.1;
+        temp->x[1] = 0.1;
+        printf("primer if yey");
+    }
+    else if ((temp->x[0] == 0) && (temp->x[1] == 1))
+    {
+        Y = 1;
+        temp->x[0] = 0.1;
+        printf("if 0 y 1");
+    }
+    else if ((temp->x[0] == 1) && (temp->x[1] == 0))
+    {
+        Y = 1;
+        temp->x[1] = 0.1;
+        printf("if 1 y 0");
+    }
+    else if ((temp->x[0] == 1) && (temp->x[1] == 1))
+    {
+        Y = 1;
+        printf("if 1 y 1");
+    }
 
     while (i <= 500)
     {
-        if ((temp->x[0] == 0.001) && (temp->x[1] == 0.001))
-            Y = 0;
-        else if ((temp->x[0] == 0.001) && (temp->x[1] == 1))
-            Y = 1;
-        else if ((temp->x[0] == 1) && (temp->x[1] == 0.001))
-            Y = 1;
-        else if ((temp->x[0] == 1) && (temp->x[1] == 1))
-            Y = 1;
-
         Z = (temp->w[0] * temp->x[0]) + (temp->w[1] * temp->x[1]);
-        Yp = (1) / (1 + exp(Z));
-        E = (Yp - Y);
+        Yp = (1) / (exp(-1 * Z) + 1);
+        E = (Y - Yp);
         temp->w[0] = temp->w[0] + (n * E * temp->x[0]);
         temp->w[1] = temp->w[1] + (n * E * temp->x[1]);
-        printf("%d salida -> %f\n", i, Yp);
+        printf("%d salida -> %f\nE %f\n", i, Yp, Y);
 
         if (Yp < 0.2)
-            printf("final 0 \n");
+            printf("Final 0 \n");
 
         if (Yp > 0.8)
-            printf("final 1 \n");
+            printf("Final 1 \n");
 
         i++;
     }
@@ -77,40 +88,50 @@ void AND(neural **Inicio)
     n = 0.5;
     temp->w[0] = 0.5;
     temp->w[1] = 0.5;
-    printf("Inserta tu 1era entrada(0 o 1):\n");
+    printf("Inserta tu 1era entrada(0 o 1): ");
     scanf("%f", &temp->x[0]);
-    printf("Inserta tu 2nda entrada(0 o 1):\n");
+    printf("Inserta tu 2nda entrada(0 o 1): ");
     scanf("%f", &temp->x[1]);
 
-    if (temp->x[0] == 0)
-        temp->x[0] = 0.001;
-
-    if (temp->x[1] == 0)
-        temp->x[1] = 0.001;
-
-    while (i <= 200)
+    if ((temp->x[0] == 0) && (temp->x[1] == 0))
     {
-        if ((temp->x[0] == 0.001) && (temp->x[1] == 0.001))
-            Y = 0;
-        else if ((temp->x[0] == 0.001) && (temp->x[1] == 1))
-            Y = 0;
-        else if ((temp->x[0] == 1) && (temp->x[1] == 0.001))
-            Y = 0;
-        else if ((temp->x[0] == 1) && (temp->x[1] == 1))
-            Y = 1;
+        Y = 0;
+        temp->x[0] = 0.1;
+        temp->x[1] = 0.1;
+        printf("primer if yey");
+    }
+    else if ((temp->x[0] == 0) && (temp->x[1] == 1))
+    {
+        Y = 0;
+        temp->x[0] = 0.1;
+        printf("if 0 y 1");
+    }
+    else if ((temp->x[0] == 1) && (temp->x[1] == 0))
+    {
+        Y = 0;
+        temp->x[1] = 0.1;
+        printf("if 1 y 0");
+    }
+    else if ((temp->x[0] == 1) && (temp->x[1] == 1))
+    {
+        Y = 1;
+        printf("if 1 y 1");
+    }
 
+    while (i <= 500)
+    {
         Z = (temp->w[0] * temp->x[0]) + (temp->w[1] * temp->x[1]);
-        Yp = (1) / (1 + exp(Z));
-        E = (Yp - Y);
+        Yp = (1) / (exp(-1 * Z) + 1);
+        E = (Y - Yp);
         temp->w[0] = temp->w[0] + (n * E * temp->x[0]);
         temp->w[1] = temp->w[1] + (n * E * temp->x[1]);
         printf("%d salida -> %f\n", i, Yp);
 
         if (Yp < 0.2)
-            printf("final 0 \n");
+            printf("Final 0 \n");
 
         if (Yp > 0.8)
-            printf("final 1 \n");
+            printf("Final 1 \n");
 
         i++;
     }
