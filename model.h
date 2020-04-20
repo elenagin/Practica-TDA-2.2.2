e1167e450b9dc9ec37222c515ecb9a88a0fffa41
lennygin <lennginzan@gmail.com>
2020-04-19 19:39:55 -0500

    XNOR working

--- model.h
+++ model.h
@@ -206,7 +206,6 @@ void XOR(neural **Inicio, neural **Inicio2)
 
         i++;
     }
-    i = 0;
 }
 
 void NOT(neural **Inicio)
@@ -251,5 +250,71 @@ void NOT(neural **Inicio)
 
 void XNOR(neural **Inicio, neural **Inicio2)
 {
-    printf("XNOR");
+    float n1 = 0, n2 = 0, E = 0, Y, Yp1 = 0, Yp2 = 0, Z1 = 0, Z2 = 0, Ew0 = 0, Ew1 = 0;
+    int i = 0;
+
+    neural *temp[2];
+
+    temp[0] = *Inicio;
+    temp[1] = *Inicio2;
+    n1 = 0.5;
+    n2 = 0.5;
+    temp[0]->w[0] = 0.5;
+    temp[0]->w[1] = 0.5;
+    temp[1]->w[0] = 0.5;
+    temp[1]->w[1] = 0.5;
+    printf("Inserta tu 1era entrada(0 o 1): ");
+    scanf("%f", &temp[0]->x[0]);
+    printf("Inserta tu 2nda entrada(0 o 1): ");
+    scanf("%f", &temp[0]->x[1]);
+
+    if ((temp[0]->x[0] == 0) && (temp[0]->x[1] == 0))
+    {
+        Y = 1;
+        temp[0]->x[0] = 0.1;
+        temp[0]->x[1] = 0.1;
+    }
+    else if ((temp[0]->x[0] == 0) && (temp[0]->x[1] == 1))
+    {
+        Y = 0;
+        temp[0]->x[0] = 0.1;
+    }
+    else if ((temp[0]->x[0] == 1) && (temp[0]->x[1] == 0))
+    {
+        Y = 0;
+        temp[0]->x[1] = 0.1;
+    }
+    else if ((temp[0]->x[0] == 1) && (temp[0]->x[1] == 1))
+    {
+        Y = 1;
+    }
+
+    temp[1]->x[0] = temp[0]->x[0];
+
+    while (i < 1300)
+    {
+        Z1 = (temp[0]->w[0] * temp[0]->x[0]) + (temp[0]->w[1] * temp[0]->x[1]);
+        Yp1 = (1) / (exp(-1 * Z1) + 1);
+        temp[1]->x[1] = Yp1;
+        Z2 = (temp[1]->w[0] * temp[1]->x[0]) + (temp[1]->w[1] * temp[1]->x[1]);
+        Yp2 = (1) / (exp(-1 * Z2) + 1);
+        E = Y - Yp2;
+        Ew0 = (temp[0]->w[0] / (temp[0]->w[0] + temp[0]->w[1])) * E;
+        Ew1 = (temp[0]->w[1] / (temp[0]->w[0] + temp[0]->w[1])) * E;
+
+        temp[1]->w[0] = temp[1]->w[0] + (n1 * E * temp[1]->x[0]);
+        temp[1]->w[1] = temp[1]->w[1] + (n1 * E * temp[1]->x[1]);
+        temp[0]->w[0] = temp[0]->w[0] + (n1 * Ew0 * temp[0]->x[0]);
+        temp[0]->w[1] = temp[0]->w[1] + (n1 * Ew1 * temp[0]->x[1]);
+
+        printf("%d salida -> %f\n", i, Yp2);
+
+        if (Yp2 < 0.2)
+            printf("Final 0 \n");
+
+        if (Yp2 > 0.8)
+            printf("Final 1 \n");
+
+        i++;
+    }
 }
