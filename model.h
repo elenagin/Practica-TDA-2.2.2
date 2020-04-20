de3091b0716bfba5cf319a5fbd35110627c99695
lennygin <lennginzan@gmail.com>
2020-04-19 20:23:21 -0500

    fixing prints

--- model.h
+++ model.h
@@ -21,6 +21,7 @@ neural *new_neural()
 
 void OR(neural **Inicio)
 {
+    system("clear");
     float n = 0, E = 0, Y, Yp = 0, Z = 0;
     int i = 0;
 
@@ -39,24 +40,24 @@ void OR(neural **Inicio)
         Y = 0;
         temp->x[0] = 0.1;
         temp->x[1] = 0.1;
-        printf("primer if yey");
+        //printf("primer if yey");
     }
     else if ((temp->x[0] == 0) && (temp->x[1] == 1))
     {
         Y = 1;
         temp->x[0] = 0.1;
-        printf("if 0 y 1");
+        //printf("if 0 y 1");
     }
     else if ((temp->x[0] == 1) && (temp->x[1] == 0))
     {
         Y = 1;
         temp->x[1] = 0.1;
-        printf("if 1 y 0");
+        //printf("if 1 y 0");
     }
     else if ((temp->x[0] == 1) && (temp->x[1] == 1))
     {
         Y = 1;
-        printf("if 1 y 1");
+        //printf("if 1 y 1");
     }
 
     while (i <= 500)
@@ -66,20 +67,20 @@ void OR(neural **Inicio)
         E = (Y - Yp);
         temp->w[0] = temp->w[0] + (n * E * temp->x[0]);
         temp->w[1] = temp->w[1] + (n * E * temp->x[1]);
-        printf("%d salida -> %f\nE %f\n", i, Yp, Y);
-
-        if (Yp < 0.2)
-            printf("Final 0 \n");
-
-        if (Yp > 0.8)
-            printf("Final 1 \n");
+        //printf("%d salida -> %f\nE %f\n", i, Yp, Y);
 
         i++;
     }
+    if (Yp < 0.2)
+        printf("0 \n");
+
+    if (Yp > 0.8)
+        printf("1 \n");
 }
 
 void AND(neural **Inicio)
 {
+    system("clear");
     float n = 0, E = 0, Y, Yp = 0, Z = 0;
     int i = 0;
 
@@ -98,24 +99,20 @@ void AND(neural **Inicio)
         Y = 0;
         temp->x[0] = 0.1;
         temp->x[1] = 0.1;
-        printf("primer if yey");
     }
     else if ((temp->x[0] == 0) && (temp->x[1] == 1))
     {
         Y = 0;
         temp->x[0] = 0.1;
-        printf("if 0 y 1");
     }
     else if ((temp->x[0] == 1) && (temp->x[1] == 0))
     {
         Y = 0;
         temp->x[1] = 0.1;
-        printf("if 1 y 0");
     }
     else if ((temp->x[0] == 1) && (temp->x[1] == 1))
     {
         Y = 1;
-        printf("if 1 y 1");
     }
 
     while (i <= 500)
@@ -125,20 +122,21 @@ void AND(neural **Inicio)
         E = (Y - Yp);
         temp->w[0] = temp->w[0] + (n * E * temp->x[0]);
         temp->w[1] = temp->w[1] + (n * E * temp->x[1]);
-        printf("%d salida -> %f\n", i, Yp);
-
-        if (Yp < 0.2)
-            printf("Final 0 \n");
-
-        if (Yp > 0.8)
-            printf("Final 1 \n");
+        //printf("%d salida -> %f\n", i, Yp);
 
         i++;
     }
+
+    if (Yp < 0.2)
+        printf("0 \n");
+
+    if (Yp > 0.8)
+        printf("1 \n");
 }
 
 void XOR(neural **Inicio, neural **Inicio2)
 {
+    system("clear");
     float n1 = 0, n2 = 0, E = 0, Y, Yp1 = 0, Yp2 = 0, Z1 = 0, Z2 = 0, Ew0 = 0, Ew1 = 0;
     int i = 0;
 
@@ -196,20 +194,20 @@ void XOR(neural **Inicio, neural **Inicio2)
         temp[0]->w[0] = temp[0]->w[0] + (n1 * Ew0 * temp[0]->x[0]);
         temp[0]->w[1] = temp[0]->w[1] + (n1 * Ew1 * temp[0]->x[1]);
 
-        printf("%d salida -> %f\n", i, Yp2);
-
-        if (Yp2 < 0.2)
-            printf("Final 0 \n");
-
-        if (Yp2 > 0.8)
-            printf("Final 1 \n");
+        //printf("%d salida -> %f\n", i, Yp2);
 
         i++;
     }
+    if (Yp2 < 0.2)
+        printf("0 \n");
+
+    if (Yp2 > 0.8)
+        printf("1 \n");
 }
 
 void NOT(neural **Inicio)
 {
+    system("clear");
     float n = 0, E = 0, Y, Yp = 0, Z = 0;
     int i = 0;
 
@@ -236,20 +234,20 @@ void NOT(neural **Inicio)
         Yp = (1) / (exp(-1 * Z) + 1);
         E = (Y - Yp);
         temp->w[0] = temp->w[0] + (n * E * temp->x[0]);
-        printf("\n%d salida -> %f\n", i, Yp);
-
-        if (Yp < 0.2)
-            printf("Final 0 \n");
-
-        if (Yp > 0.8)
-            printf("Final 1 \n");
+        //printf("\n%d salida -> %f\n", i, Yp);
 
         i++;
     }
+    if (Yp < 0.2)
+        printf("0 \n");
+
+    if (Yp > 0.8)
+        printf("1 \n");
 }
 
 void XNOR(neural **Inicio, neural **Inicio2)
 {
+    system("clear");
     float n1 = 0, n2 = 0, E = 0, Y, Yp1 = 0, Yp2 = 0, Z1 = 0, Z2 = 0, Ew0 = 0, Ew1 = 0;
     int i = 0;
 
@@ -307,14 +305,13 @@ void XNOR(neural **Inicio, neural **Inicio2)
         temp[0]->w[0] = temp[0]->w[0] + (n1 * Ew0 * temp[0]->x[0]);
         temp[0]->w[1] = temp[0]->w[1] + (n1 * Ew1 * temp[0]->x[1]);
 
-        printf("%d salida -> %f\n", i, Yp2);
-
-        if (Yp2 < 0.2)
-            printf("Final 0 \n");
-
-        if (Yp2 > 0.8)
-            printf("Final 1 \n");
+        //printf("%d salida -> %f\n", i, Yp2);
 
         i++;
     }
+    if (Yp2 < 0.2)
+        printf("0 \n");
+
+    if (Yp2 > 0.8)
+        printf("1 \n");
 }
